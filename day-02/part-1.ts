02d5b70840ff30885af443d88dfa89bfd2a5c954
vrugtehagel <vrugtehagel@pm.me>
2024-12-02 15:32:59 +0100

    Day 2

---
+++ day-02/part-1.ts
@@ -0,0 +1,21 @@
+const input = await Deno.readTextFile(new URL('./input.txt', import.meta.url))
+
+// Parse it into an array of individual reports
+const reports: Array<number[]> = input.trim().split('\n')
+	.map(line => line.split(/\s+/).map(number => Number(number)))
+
+// Now count the number of safe reports
+let safeReports = 0
+for(const report of reports){
+	const ascending = report[0] < report[1]
+		? [...report]
+		: report.toReversed()
+	const differences = ascending.slice(0, -1)
+		.map((value, index) => ascending[index + 1] - value)
+	const safe = differences
+		.every(difference => 1 <= difference && difference <= 3)
+	if(safe) safeReports++
+}
+
+// Done!
+console.log(safeReports)
