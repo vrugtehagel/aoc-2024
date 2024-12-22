f3825e58dcc814189cc817d603f26d8eb88317ac
vrugtehagel <vrugtehagel@pm.me>
2024-12-22 13:17:55 +0100

    Faster solution

--- day-22/part-2.ts
+++ day-22/part-2.ts
@@ -5,32 +5,33 @@ export function solution(input: string): number {
 	// The recipe to "randomize" a secret
 	function randomize(secret: number): number {
 		secret = ((secret << 6) ^ secret) & 0xFFFFFF
-		secret = ((secret >>> 5) ^ secret) & 0xFFFFFF
+		secret = (secret >>> 5) ^ secret
 		secret = ((secret << 11) ^ secret) & 0xFFFFFF
 		return secret
 	}
 
 	// This time, we keep track of the last 4 differences in price, and add it
-	// to a map (only if it hasn't been seen before). Then, we aggregate the
-	// scores into this "scores" map. The one with the highest score wins!
-	const scores = new Map<string, number>()
+	// to a map (only if it hasn't been seen before). Using an array for the
+	// last 4 items, and converting them to a string to use as map index, is
+	// very slow. So, instead, we use a single integer to represent the last 4
+	// differences. Specifically we use a base-32 integer where each digit
+	// is a difference plus 9 (to make it >= 0). This means we can use bit
+	// operations to push and shift items on and off of the last 4, and then
+	// directly use the number as map index.
+	const scores = new Map<number, number>()
 	for (let secret of secrets) {
-		const last4 = []
-		let price
-		const bananas = new Map<string, number>()
-		for (let run = 0; run <= 2000; run++) {
-			if (price != null) last4.push(secret % 10 - price)
-			price = secret % 10
+		let last4 = 0
+		let price = secret % 10
+		const seen = new Set<number>()
+		for (let run = 1; run <= 2000; run++) {
 			secret = randomize(secret)
-			if (last4.length > 4) last4.shift()
-			if (last4.length < 4) continue
+			last4 = ((last4 << 5) & 0xFFFFF) + 9 + secret % 10 - price
+			price = secret % 10
 			if (price == 0) continue
-			const diffs = `${last4}`
-			if (bananas.has(diffs)) continue
-			bananas.set(diffs, price)
-		}
-		for (const [diffs, score] of bananas) {
-			scores.set(diffs, (scores.get(diffs) ?? 0) + score)
+			if (run < 4) continue
+			if (seen.has(last4)) continue
+			seen.add(last4)
+			scores.set(last4, (scores.get(last4) ?? 0) + price)
 		}
 	}
 
