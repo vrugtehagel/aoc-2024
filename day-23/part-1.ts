05d478885fb4eaaf178638cb86f41571d9dcba58
vrugtehagel <vrugtehagel@pm.me>
2024-12-24 00:04:49 +0100

    Day 23

---
+++ day-23/part-1.ts
@@ -0,0 +1,32 @@
+export function solution(input: string): number {
+	// First, we parse our input into a map that maps a computer to all the
+	// other computers it is connected to. A computer is represented as the
+	// given two-character string in the input.
+	const computers = new Map<string, Set<string>>()
+	const rawLinks = input.trim().split('\n')
+	for (const rawLink of rawLinks) {
+		const [from, to] = rawLink.split('-')
+		computers.get(from)?.add(to) ?? computers.set(from, new Set([to]))
+		computers.get(to)?.add(from) ?? computers.set(to, new Set([from]))
+	}
+
+	// Now we create a set of all the networks that include a t-computer. We add
+	// each three interconnected computer triple to this "networks" set, as a
+	// sorted set of dash-concatenated computer IDs (e.g. 'co-ka-te'). This also
+	// helps avoid duplicates.
+	const networks = new Set<string>()
+	for (const [computer1, connections] of computers) {
+		for (const computer2 of connections) {
+			const overlap = computers.get(computer2).intersection(connections)
+			for (const computer3 of overlap) {
+				const computers = [computer1, computer2, computer3]
+				if (!computers.some((computer) => computer[0] == 't')) continue
+				const network = computers.sort().join('-')
+				networks.add(network)
+			}
+		}
+	}
+
+	// And done! Return the number of networks.
+	return networks.size
+}
---
+++ day-23/part-2.ts
@@ -0,0 +1,63 @@
+export function solution(input: string): string {
+	// Just like part 1, we parse our input into a mapping of each computer to
+	// the set of every other computer it is connected to. This time, we include
+	// a computer within the connected computers it maps to.
+	const computers = new Map<string, Set<string>>()
+	const rawLinks = input.trim().split('\n')
+	for (const rawLink of rawLinks) {
+		const [from, to] = rawLink.split('-')
+		computers.get(from)?.add(to) ?? computers.set(from, new Set([to]))
+		computers.get(to)?.add(from) ?? computers.set(to, new Set([from]))
+		computers.get(from).add(from)
+		computers.get(to).add(to)
+	}
+
+	// Checks if a set of computers are all individually connected to each other
+	function isNetwork(group: Set<string>): boolean {
+		for (const computer of group) {
+			const connected = computers.get(computer)
+			if (!group.isSubsetOf(connected)) return false
+		}
+		return true
+	}
+
+	// Try to find a network of a certain size within a group of computers. This
+	// function is recursive; it tries all possible subsets of group that have
+	// the specified size. Once it finds a network, it stops looking and returns
+	// it. If there are none, it returns null.
+	function findNetworkWithin(
+		group: Set<string>,
+		size: number,
+	): null | Set<string> {
+		if (group.size == size) return isNetwork(group) ? group : null
+		for (const computer of group) {
+			const smaller = new Set(group)
+			smaller.delete(computer)
+			const found = findNetworkWithin(smaller, size)
+			if (found) return found
+		}
+		return null
+	}
+
+	// Finds a network of a certain size within the input data.
+	function findNetwork(size: number): null | Set<string> {
+		for (const [computer, connected] of computers) {
+			const network = findNetworkWithin(connected, size)
+			if (network) return network
+		}
+		return null
+	}
+
+	// We try to find a connected network, starting from the largest possible
+	// size. Then, if a network of a certain size doesn't exist, we start over
+	// but look for a size that is 1 smaller than before.
+	let size = Math.max(...[...computers.values()].map(({ size }) => size))
+	while (size > 0) {
+		const network = findNetwork(size)
+		if (network) return [...network].sort().join(',')
+		size--
+	}
+
+	// This can't happen because we know already there are networks of size 2
+	return null
+}
